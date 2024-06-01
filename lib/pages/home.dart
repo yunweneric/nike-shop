53a7f93b2216b5876d9c676a7f62cc3a8a6090d2
yunweneric <yunweneric@gmail.com>
2024-06-01 19:37:40 +0100

    Implemented step counter

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_nike_shop/pages/data.dart';
 import 'package:flutter_3d_nike_shop/utils/asset_helper.dart';
 import 'package:flutter_3d_nike_shop/utils/colors.dart';
 import 'package:flutter_3d_nike_shop/utils/sizing.dart';
@@ -19,36 +20,6 @@ class _HomePageState extends State<HomePage> {
   PageController controller = PageController();
   PageController controller_2 = PageController();
   int activeIndex = 0;
-
-  List<CartItem> items = [
-    CartItem(
-      index: 0,
-      title: "Nike Air Max Pre-Day",
-      desc:
-          "Taking the classic look of heritage Nike Running into a new realm, the Nike Air Max Pre-Day brings you a fast-paced look that's ready for today's world.A true nod to the past with a design made from at least 20% recycled material by weight.",
-      price: "55,000 XAF",
-      main: AppColors.bGreen,
-      light: AppColors.lightGreen,
-    ),
-    CartItem(
-      index: 1,
-      title: "Nike Air Max Pre-Day",
-      desc:
-          "Taking the classic look of heritage Nike Running into a new realm, the Nike Air Max Pre-Day brings you a fast-paced look that's ready for today's world.A true nod to the past with a design made from at least 20% recycled material by weight.",
-      price: "30,000 XAF",
-      main: AppColors.bgRed,
-      light: AppColors.lightRed,
-    ),
-    CartItem(
-      index: 2,
-      title: "Nike Air Max Pre-Day",
-      desc:
-          "Taking the classic look of heritage Nike Running into a new realm, the Nike Air Max Pre-Day brings you a fast-paced look that's ready for today's world.A true nod to the past with a design made from at least 20% recycled material by weight.",
-      price: "73,000 XAF",
-      main: AppColors.bgGray,
-      light: AppColors.lightGray,
-    )
-  ];
 
   final duration = const Duration(milliseconds: 1200);
   @override
@@ -97,7 +68,7 @@ class _HomePageState extends State<HomePage> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
+                        SizedBox(
                           width: Sizing.width(context) * 0.3,
                           height: 300,
                           child: PageView.builder(
@@ -143,7 +114,7 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 30),
+                              margin: const EdgeInsets.only(left: 30),
                               child: Text(
                                 items[activeIndex].price,
                                 style: Theme.of(context).textTheme.displayMedium!.copyWith(color: AppColors.white, fontSize: 28),
@@ -161,19 +132,23 @@ class _HomePageState extends State<HomePage> {
               offset: Offset(Sizing.width(context) / 5, 0),
               child: Container(
                 height: Sizing.height(context),
-                // color: Colors.teal,
                 alignment: Alignment.centerRight,
                 width: Sizing.width(context),
                 child: PageView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
                   controller: controller,
                   itemCount: items.length,
                   scrollDirection: Axis.vertical,
                   itemBuilder: (c, i) {
-                    return Transform(
-                      transform: Matrix4.identity()
-                        ..setEntry(3, 2, 0.01)
-                        ..scale(1.0),
-                      child: Image.asset("assets/images/shoe_${i}.png"),
+                    return AnimatedOpacity(
+                      duration: duration,
+                      opacity: i == activeIndex ? 1 : 0,
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.01)
+                          ..scale(1.0),
+                        child: Image.asset("assets/images/shoe_${i}.png"),
+                      ),
                     );
                   },
                 ),
@@ -185,15 +160,17 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void animateChange(int index) async {
+    setState(() {
+      activeIndex = index;
+    });
+    await controller.animateToPage(index, duration: Duration(milliseconds: 1500), curve: Curves.easeInOutBack);
+    controller_2.animateToPage(index, duration: Duration(milliseconds: 800), curve: Curves.ease);
+  }
+
   Widget indicator(bool isActive, int index) {
     return InkWell(
-      onTap: () async {
-        setState(() {
-          activeIndex = index;
-        });
-        await controller.animateToPage(index, duration: Duration(milliseconds: 1500), curve: Curves.easeInOutBack);
-        controller_2.animateToPage(index, duration: Duration(milliseconds: 800), curve: Curves.ease);
-      },
+      onTap: () => animateChange(index),
       child: Row(
         children: [
           AnimatedContainer(
@@ -201,7 +178,7 @@ class _HomePageState extends State<HomePage> {
             height: activeIndex == index ? 120 : 80,
             width: 4,
             color: isActive ? AppColors.white : AppColors.white.withOpacity(0.5),
-            margin: EdgeInsets.only(right: 25),
+            margin: const EdgeInsets.only(right: 25),
           ),
           Text(
             "0${index + 1}",
@@ -211,22 +188,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
-
-class CartItem {
-  final int index;
-  final String title;
-  final String desc;
-  final String price;
-  final Color main;
-  final Color light;
-
-  CartItem({
-    required this.index,
-    required this.title,
-    required this.desc,
-    required this.price,
-    required this.main,
-    required this.light,
-  });
 }
